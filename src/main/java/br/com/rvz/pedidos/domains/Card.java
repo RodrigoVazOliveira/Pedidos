6039e8cc711e7ee58ade33e66049247610d2dfb7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-07-31 03:14:18 -0300

    Criar modelo de cartoes

---
+++ src/main/java/br/com/rvz/pedidos/domains/Card.java
@@ -0,0 +1,75 @@
+package br.com.rvz.pedidos.domains;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table("cartoes")
+public class Card {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Long number;
+	
+	@Column(nullable = false)
+	private LocalDate dataValidatation;
+	
+	@Column(length = 60, nullable = false)
+	private String nameHolder;
+	
+	@Column(length = 3)
+	private Integer cvv;
+
+	public Card() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumber() {
+		return number;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public LocalDate getDataValidatation() {
+		return dataValidatation;
+	}
+
+	public void setDataValidatation(LocalDate dataValidatation) {
+		this.dataValidatation = dataValidatation;
+	}
+
+	public String getNameHolder() {
+		return nameHolder;
+	}
+
+	public void setNameHolder(String nameHolder) {
+		this.nameHolder = nameHolder;
+	}
+
+	public Integer getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(Integer cvv) {
+		this.cvv = cvv;
+	}
+}
